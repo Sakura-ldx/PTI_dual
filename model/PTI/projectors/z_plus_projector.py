6cbd898141d342bf084e0adf8fb01c4835500650
sakura <981285984@qq.com>
2022-04-18 20:31:52 +0800

    first commit

---
+++ model/PTI/projectors/z_plus_projector.py
@@ -0,0 +1,130 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+"""Project given image to the latent space of pretrained network pickle."""
+
+import copy
+import numpy as np
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+import model.PTI.dnnlib as dnnlib
+
+
+def project(
+        G,
+        target: torch.Tensor,  # [C,H,W] and dynamic range [0,255], W & H must match G output resolution
+        *,
+        num_steps=1000,
+        w_avg_samples=10000,
+        initial_learning_rate=0.01,
+        initial_noise_factor=0.05,
+        lr_rampdown_length=0.25,
+        lr_rampup_length=0.05,
+        noise_ramp_length=0.75,
+        regularize_noise_weight=1e5,
+        verbose=False,
+        device: torch.device,
+        initial_z=None
+):
+
+    def logprint(*args):
+        if verbose:
+            print(*args)
+
+    G = copy.deepcopy(G).eval().requires_grad_(False).to(device).float() # type: ignore
+
+    # Compute w stats.
+    logprint(f'Computing W midpoint and stddev using {w_avg_samples} samples...')
+    z_samples = np.random.RandomState(123).randn(w_avg_samples, G.style_dim)
+    w_samples = G.get_latent(torch.from_numpy(z_samples).float().to(device)).unsqueeze(1)   # [N, L, C]
+    w_samples = w_samples[:, :1, :].cpu().numpy().astype(np.float32)  # [N, 1, C]
+    w_avg = np.mean(w_samples, axis=0, keepdims=True)  # [1, 1, C]
+    w_avg_tensor = torch.from_numpy(w_avg).to(device)
+    w_std = (np.sum((w_samples - w_avg) ** 2) / w_avg_samples) ** 0.5
+
+    start_z = initial_z
+
+    # Setup noise inputs.
+    # noise_bufs = {name: buf for (name, buf) in G.synthesis.named_buffers() if 'noise_const' in name}
+
+    # Load VGG16 feature detector.
+    url = 'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metrics/vgg16.pt'
+    with dnnlib.util.open_url(url) as f:
+        vgg16 = torch.jit.load(f).eval().to(device)
+
+    # Features for target image.
+    target_images = target.unsqueeze(0).to(device).to(torch.float32)
+    if target_images.shape[2] > 256:
+        target_images = F.interpolate(target_images, size=(256, 256), mode='area')
+    target_features = vgg16(target_images, resize_images=False, return_lpips=True)
+
+    # pylint: disable=not-callable
+    z_opt = torch.tensor(start_z, dtype=torch.float32, device=device, requires_grad=True).unsqueeze(0)
+
+    # Init noise.
+    noises = G.make_noise()
+    for noise in noises:
+        noise.requires_grad = True
+
+    optimizer = torch.optim.Adam([z_opt] + noises, betas=(0.9, 0.999), lr=5e-3)
+
+    for step in tqdm(range(num_steps)):
+        w_opt = G.get_latents(z_opt)
+
+        # Learning rate schedule.
+        t = step / num_steps
+        w_noise_scale = w_std * initial_noise_factor * max(0.0, 1.0 - t / noise_ramp_length) ** 2
+        lr_ramp = min(1.0, (1.0 - t) / lr_rampdown_length)
+        lr_ramp = 0.5 - 0.5 * np.cos(lr_ramp * np.pi)
+        lr_ramp = lr_ramp * min(1.0, t / lr_rampup_length)
+        lr = initial_learning_rate * lr_ramp
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+        # Synth images from opt_w.
+        w_noise = torch.randn_like(w_opt) * w_noise_scale
+        ws = (w_opt + w_noise)
+
+        synth_images, _ = G([ws], input_is_latent=True, noise=noises, return_latents=False)
+
+        # Downsample image to 256x256 if it's larger than that. VGG was built for 224x224 images.
+        synth_images = (synth_images + 1) * (255 / 2)
+        if synth_images.shape[2] > 256:
+            synth_images = F.interpolate(synth_images, size=(256, 256), mode='area')
+
+        # Features for synth images.
+        synth_features = vgg16(synth_images, resize_images=False, return_lpips=True)
+        dist = (target_features - synth_features).square().sum()
+
+        # Noise regularization.
+        reg_loss = 0.0
+        for noise in noises:  # must be [1,1,H,W] for F.avg_pool2d()
+            while True:
+                reg_loss += (noise * torch.roll(noise, shifts=1, dims=3)).mean() ** 2
+                reg_loss += (noise * torch.roll(noise, shifts=1, dims=2)).mean() ** 2
+                if noise.shape[2] <= 8:
+                    break
+                noise = F.avg_pool2d(noise, kernel_size=2)
+        loss = dist + reg_loss * regularize_noise_weight
+
+        # Step
+        optimizer.zero_grad(set_to_none=True)
+        loss.requires_grad = True
+        loss.backward()
+        optimizer.step()
+        logprint(f'step {step + 1:>4d}/{num_steps}: dist {dist:<4.2f} loss {float(loss):<5.2f}')
+
+        # Normalize noise.
+        with torch.no_grad():
+            for i, noise in enumerate(noises):
+                noises[i] -= noise.mean()
+                noises[i] *= noise.square().mean().rsqrt()
+
+    del G
+    return z_opt
---
+++ model/PTI/pti.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+from lpips import LPIPS
+from tqdm import tqdm
+
+from model.PTI.criteria import l2_loss
+from model.PTI.criteria.localitly_regulizer import Space_Regulizer
+from model.PTI.projectors import z_plus_projector
+from model.PTI.configs import hyperparameters
+
+
+class PTI(nn.Module):
+    def __init__(self, G, device):
+        super(PTI, self).__init__()
+        self.G = G
+        self.device = device
+        self.optimizer = torch.optim.Adam(self.G.parameters(), lr=hyperparameters.pti_learning_rate)
+
+        self.pt_l2_lambda = hyperparameters.pt_l2_lambda
+        self.pt_lpips_lambda = hyperparameters.pt_lpips_lambda
+        self.lpips_loss = LPIPS(net=hyperparameters.lpips_type).to(self.device).eval()
+        self.space_regulizer = Space_Regulizer(self.G, self.lpips_loss)
+
+        self.use_wandb = False
+
+    def forward(self, z_plus_inits, images, is_finetune):
+        z_pivots = []
+        for image, z_plus_init in zip(images, z_plus_inits):
+            id_image = torch.squeeze((image.to(self.device) + 1) / 2) * 255
+            z_plus = z_plus_projector.project(self.G, id_image, device=torch.device(self.device), w_avg_samples=600,
+                                              num_steps=450, initial_z=z_plus_init)
+            z_pivots.append(z_plus)
+
+        if is_finetune:
+            for i in tqdm(range(550)):
+
+                for image, z_pivot in zip(images, z_pivots):
+                    real_images_batch = image.to(self.device)
+
+                    generated_images, _ = self.G([z_pivot],
+                                                 input_is_latent=False,
+                                                 return_latents=False,
+                                                 z_plus_latent=True)
+                    loss, l2_loss_val, loss_lpips = self.calc_loss(generated_images, real_images_batch,
+                                                                   self.G, False, z_pivot)
+
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+
+                    # use_ball_holder = global_config.training_step % hyperparameters.locality_regularization_interval == 0
+
+                    # global_config.training_step += 1
+
+        return z_pivots, self.G
+
+    def calc_loss(self, generated_images, real_images, new_G, use_ball_holder, w_batch):
+        loss = 0.0
+
+        if self.pt_l2_lambda > 0:
+            l2_loss_val = l2_loss.l2_loss(generated_images, real_images)
+            loss += l2_loss_val * self.pt_l2_lambda
+        if self.pt_lpips_lambda > 0:
+            loss_lpips = self.lpips_loss(generated_images, real_images)
+            loss_lpips = torch.squeeze(loss_lpips)
+            loss += loss_lpips * self.pt_lpips_lambda
+        if use_ball_holder and self.use_locality_regularization:
+            ball_holder_loss_val = self.space_regulizer.space_regulizer_loss(new_G, w_batch, use_wandb=self.use_wandb)
+            loss += ball_holder_loss_val
+
+        return loss, l2_loss_val, loss_lpips
